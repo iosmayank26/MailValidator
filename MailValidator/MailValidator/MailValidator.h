cac6336b9169e726e5a615652b6d88afee2a714c
Mayank Gupta <mayank.gupta679@gmail.com>
2021-06-25 17:23:18 +0530

    add framework

---
+++ MailValidator/MailValidator/MailValidator.h
@@ -0,0 +1,18 @@
+//
+//  MailValidator.h
+//  MailValidator
+//
+//  Created by Mayank Gupta on 25/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MailValidator.
+FOUNDATION_EXPORT double MailValidatorVersionNumber;
+
+//! Project version string for MailValidator.
+FOUNDATION_EXPORT const unsigned char MailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailValidator/PublicHeader.h>
+
+
